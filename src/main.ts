fad4f01b15cb9e604aa440b7ee8ef7600aac66da
Rafael Monteiro <rafaelmonteiroarj@gmail.com>
2022-10-12 22:56:25 -0300

    ✨ Adicionando configuração de log's.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { WinstonModule } from 'nest-winston';
+
 import { AppModule } from './app.module';
+import { winstonConfig } from './config/ winston';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = WinstonModule.createLogger(winstonConfig);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger,
+  });
+
+  await app.listen(PORT, () =>
+    logger.log(`Application is running on port ${PORT}`),
+  );
 }
 bootstrap();
