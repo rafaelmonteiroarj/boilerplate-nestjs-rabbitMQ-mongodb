56fa6c6d6acf41391e2949034f59fb7a60cec825
Rafael Monteiro <rafaelmonteiroarj@gmail.com>
2022-10-14 23:22:31 -0300

    ✨ adding mongoose configuration and environment variables configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
 
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: !ENV ? '.env' : `.env.${ENV}` }),
+    MongooseModule.forRoot(`${process.env.MONGODB_URI}`, {
+      useNewUrlParser: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
